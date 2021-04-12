6bf6c514d42b6d7d80ea6235a044aff9c842b15a
TotallyNotRust <35370673+TotallyNotRust@users.noreply.github.com>
2021-04-12 10:16:21 +0200

    Clean code optimering af reaction game
    - Tilføjet class til spillere som holder deres navn og om de er færdige.
    - Spiller classen holder også en metode der udregner reaktionstid, istedet for at koden skrives 2 gang.
    Da python ikke har scopes på samme måde som f.eks c++, kan classes bruges som alternativ.

--- Grund. progam/reactionGame.py
+++ Grund. progam/reactionGame.py
@@ -1,39 +1,44 @@
 try:
     import random, keyboard
     from datetime import datetime
-    from time import sleep
 except NameError:
     print("Please run this command")
     print("python -m pip install -r requirements.txt")
     input()
     exit()
-now = datetime.now
 
-timeNow = now()
+timeNow = datetime.now()
 
 time = random.randint(1, 11)
 
 print("Get ready...")
 
-failed = False
+class Player:
+    done = False
+    def __init__(self, name: str):
+        self.name = name
+    def calcLen(self, start, end, **kw): 
+        """Calculates the amount of time it takes the player to press their button"""
+        delta = end - start
+        print(f"{self.name}: {round(delta.microseconds/1000)}")
+        self.done = True
+    
 
-while (now() - timeNow).seconds < time:
+p1, p2 = Player("Player 1"), Player("Player 2")
+
+while (datetime.now() - timeNow).seconds < time:
     if keyboard.is_pressed("enter"):
-        print("You pressed too early")
-        failed = True
-        break
+        print("Player 1 pressed too early")
+        p1.done = True
+    elif keyboard.is_pressed("space"):
+        print("Player 2 pressed too early")
+        p2.done = True
 
-p1done, p2done = False, False
-
-if not failed:
-    print("0")
-    start = now()
-    while not p1done and  not p2done:
-        if keyboard.is_pressed("enter"):
-            delta = now() - start
-            print(f"P1: {round(delta.microseconds/1000)}")
-            p1done = True
-        if keyboard.is_pressed("space"):
-            delta = now() - start
-            print(f"P2: {round(delta.microseconds/1000)}")
-            p2done = True
+print("0")
+start = datetime.now()
+while not p1.done or not p2.done:
+    if keyboard.is_pressed("enter") and not p1.done:
+        p1.calcLen(start, datetime.now())
+    if keyboard.is_pressed("space") and not p2.done:
+        p2.calcLen(start, datetime.now())
+        
